fdd65b34ce81c9d9fe10fce62d186dd90ed8691d
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-11-16 22:17:41 +0700

    add top navbar and make it responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {close, logo, menu} from "../assets/index.js";
+import {navLinks} from "../constants/index.js";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt="LooBank" className="w-[124px] h-[32px]"/>
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, idx) => (
+          <li
+            key={nav.id}
+            className={`
+              font-poppins font-normal cursor-pointer
+              text-[16px] text-white
+              ${idx === navLinks.length - 1 ? 'mr-0' : 'mr-10'}
+            `}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(prev => !prev)}
+        />
+        <div
+          className={`
+            ${toggle ? 'flex' : 'hidden'}
+            p-6 absolute bg-black-gradient top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar
+          `}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, idx) => (
+              <li
+                key={nav.id}
+                className={`
+                  font-poppins font-normal cursor-pointer text-[16px] text-white
+                  ${idx === navLinks.length - 1 ? 'mb-0' : 'mb-10'}
+                `}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    
+    </nav>
   );
 };
 
